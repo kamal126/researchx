753aa8d7aedb4a413073546c3376fa1de96d0467
kamal126 <kamalchandra265@gmail.com>
2025-11-19 20:59:48 +0530

    fix: remove empty code change entries

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,5 +1,32 @@
-export * from "./icon";
+import { ImageResponse } from "next/og";
+
+export const size = {
+  width: 64,
+  height: 64,
+};
+export const contentType = "image/png";
 
 export default function Icon() {
-  return null;
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 40,
+          background: "black",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "#E1B10D",
+          borderRadius: "12px",
+        }}
+      >
+        Rx
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
 }
